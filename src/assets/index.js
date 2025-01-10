264f4b4fc689a114425d91a4400d3f953ab6e21a
pyPrarthan <christianprarthan07@gmail.com>
2025-01-10 18:09:18 -0500

    tech logo updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,6 +7,7 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
+
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -30,7 +31,10 @@ import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
+import maletech from "./maletech.png"
+
 export {
+  maletech,
   logo,
   backend,
   creator,
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 
 import {styles} from "../styles"
 import {navLinks} from '../constants'
-import {logo, menu , close,} from '../assets'
+import { menu, close, maletech } from "../assets";
 
 const Navbar = () => {
 
@@ -24,7 +24,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <img src={maletech} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Prarthan &nbsp;
             <span className="sm:block hidden"> | Christian </span>
@@ -67,21 +67,17 @@ const Navbar = () => {
                     active === nav.title ? "text-white" : "text-secondary"
                   }font-poppins font-medium cursor-pointer text-[16px]`}
                   onClick={() => {
-                    setToggle(!toggle)
-                    setActive(nav.title)
+                    setToggle(!toggle);
+                    setActive(nav.title);
                   }}
                 >
                   <a href={`#${nav.id}`}>{nav.title}</a>
                 </li>
               ))}
             </ul>
-
           </div>
-
         </div>
-
       </div>
-      
     </nav>
   );
 }
