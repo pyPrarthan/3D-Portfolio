fe3d9083d2d26c0b8514a1bb34b12c7054763e4e
pyPrarthan <christianprarthan07@gmail.com>
2025-01-30 17:41:20 -0500

    fixed the div

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -62,14 +62,14 @@ const Works = () => {
          variants={fadeIn("", "", 0.1, 1)} className="mt-3 text-secondary text-[17px]
          max-w-3xl leading-[30px]"
         >
-          Followin projects showcases my skills and experience through real-world examples
+          Following projects showcases my skills and experience through real-world examples
           of my work. Each project is briefly described with links to code repositories and live
           demos in it. It reflects my ability to solve complex problems, work with different 
           technologies, and manage projects effectively.
         </motion.p>
       </div>
 
-      <div className="mt-20 flex flex-wrap gap-7">
+      <div className="mt-20 flex flex-wrap gap-6">
         {projects.map((project, index)=>(
           <ProjectCard
             key={`project-${index}`}
